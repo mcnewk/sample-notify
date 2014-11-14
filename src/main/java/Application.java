23dd06960b67214a2faf9f8a491b998856edb59f
Eric Westfall <ewestfal@gmail.com>
2014-11-13 22:15:06 -0500

    Committing sending of emails.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,14 +1,30 @@
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 @EnableMongoRepositories
+@EnableAsync
 public class Application {
+
+    @Value("${mail.smtp.port}")
+    private int mailSmtpPort;
+
+    @Bean
+    public MailSender mailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setPort(mailSmtpPort);
+        return mailSender;
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
--- src/main/java/notify/NotificationController.java
+++ src/main/java/notify/NotificationController.java
@@ -2,6 +2,9 @@ package notify;
 
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +21,10 @@ public class NotificationController {
     @Autowired
     private NotificationRepository repository;
 
+    @Autowired
+    private MailSender mailSender;
+
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseBody
     public Notification getNotification(@PathVariable String id) {
@@ -32,7 +39,20 @@ public class NotificationController {
     @ResponseBody
     public String send(@RequestBody Notification notification) {
         notification.setCreated(DateTime.now());
-        return repository.save(notification).getId();
+        Notification saved = repository.save(notification);
+        sendEmail(notification);
+        return saved.getId();
+    }
+
+    @Async
+    private void sendEmail(Notification notification) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(notification.getFrom());
+        message.setCc(notification.getCarbonCopies().toArray(new String[0]));
+        message.setSubject(notification.getTitle());
+        message.setText(notification.getMessage());
+        message.setTo(notification.getRecipients().toArray(new String[0]));
+        mailSender.send(message);
     }
 
 }
